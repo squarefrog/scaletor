3bc693c91b5798506517da00016b9ff187594770
Paul Williamson <squarefrog@gmail.com>
2020-07-26 11:18:21 +0100

    Add Harmonic Minor mode

--- Sources/Scaletor/Models/Interval.swift
+++ Sources/Scaletor/Models/Interval.swift
@@ -3,4 +3,5 @@ import Foundation
 public enum Interval: Int {
     case semitone
     case tone
+    case toneAndHalf
 }
---
+++ Sources/Scaletor/Models/MinorMode.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public enum MinorMode: String, CaseIterable {
+    case harmonicMinor
+}
+
+extension MinorMode: Mode {
+    public var chords: [ChordVoicing] {
+        [.minor, .diminished, .augmented, .minor, .major, .major, .diminished]
+            .offset(by: index)
+    }
+
+    public var intervals: [Interval] {
+        [.tone, .semitone, .tone, .tone, .semitone, .toneAndHalf, .semitone]
+            .offset(by: index)
+    }
+
+    private var index: Int {
+        Self.allCases.firstIndex(of: self)!
+    }
+}
