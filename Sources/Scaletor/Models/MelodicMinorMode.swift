1840b7a883640914a4f83e1278616fec7a026a72
Paul Williamson <squarefrog@gmail.com>
2020-07-26 14:40:13 +0100

    Add Melodic Minor mode

---
+++ Sources/Scaletor/Models/MelodicMinorMode.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+public enum MelodicMinorMode: String, CaseIterable {
+    case melodicMinor
+}
+
+extension MelodicMinorMode: Mode {
+    public var chords: [ChordVoicing] {
+        [.minor, .diminished, .augmented, .major, .major, .diminished, .diminished]
+            .offset(by: index)
+    }
+
+    public var intervals: [Interval] {
+        [.tone, .semitone, .tone, .tone, .tone, .tone, .semitone]
+            .offset(by: index)
+    }
+
+    private var index: Int {
+        Self.allCases.firstIndex(of: self)!
+    }
+}
