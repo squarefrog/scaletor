d5ffda02f10e5e95938499cc1d735dbd01b073da
Paul Williamson <squarefrog@gmail.com>
2020-07-26 14:47:17 +0100

    Add Lydian Augmented mode

--- Sources/Scaletor/Models/MelodicMinorMode.swift
+++ Sources/Scaletor/Models/MelodicMinorMode.swift
@@ -3,6 +3,7 @@ import Foundation
 public enum MelodicMinorMode: String, CaseIterable {
     case melodicMinor
     case dorianFlatTwo
+    case lydianAugmented
 }
 
 extension MelodicMinorMode: Mode {
