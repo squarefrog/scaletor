6199cd9627fde07bd4449a40df307c01a7eb6aba
Paul Williamson <squarefrog@gmail.com>
2020-07-29 18:55:11 +0100

    Move assertMatches to XCTestCase extension

--- Tests/ScaletorTests/Helpers.swift
+++ Tests/ScaletorTests/Helpers.swift
@@ -1,12 +1,14 @@
 import Scaletor
 import XCTest
 
-func assertMatches(_ expectation: String, file: StaticString = #file, line: UInt = #line, scale: () throws -> [Note]) {
-    do {
-        let scale = try scale()
-        let result = scale.map(\.description).joined(separator: " ")
-        XCTAssertEqual(result, expectation, file: file, line: line)
-    } catch {
-        XCTFail("Failed to generate scale \(error)", file: file, line: line)
+extension XCTestCase {
+    func assertMatches(_ expectation: String, file: StaticString = #file, line: UInt = #line, scale: () throws -> [Note]) {
+        do {
+            let scale = try scale()
+            let result = scale.map(\.description).joined(separator: " ")
+            XCTAssertEqual(result, expectation, file: file, line: line)
+        } catch {
+            XCTFail("Failed to generate scale \(error)", file: file, line: line)
+        }
     }
 }
