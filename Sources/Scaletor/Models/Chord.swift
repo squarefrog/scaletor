12c2db4ad5ee6adf539e2b7ba21552644341c2da
Paul Williamson <squarefrog@gmail.com>
2020-07-29 15:52:49 +0100

    Add ChordFormatter struct

--- Sources/Scaletor/Models/Chord.swift
+++ Sources/Scaletor/Models/Chord.swift
@@ -3,4 +3,9 @@ import Foundation
 public struct Chord: Equatable {
     public let root: Note
     public let voicing: ChordVoicing
+
+    public init(root: Note, voicing: ChordVoicing) {
+        self.root = root
+        self.voicing = voicing
+    }
 }
